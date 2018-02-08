f9804d40ed2f54589bb31cf957243c4bf93e2201
nourbayard <nour@matise.nl>
2018-02-08 11:26:04 +0100

    opdracht 4
    is klaaaaarrrrr

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -2,35 +2,46 @@
 // dit is een object literal - dit heeft key value pairs. in dit geval is init de key en function is de pair
 // create local scope - dus alle code die er staat, staat in de scope van de IIFE
 
-console.log('global scope');
+'use strict';
 
+//console.log('global scope');
 (function(){
-
   //Initialize application
   var app = {
     //hieronder is een method
     init: function(){
-      // console.log('app initialized')
-      routes.init()
+      //console.log('app initialized');
+      //console.log(location.hash);
+      routes.init(); // hier in moet de hashchange
     }
   }
-
-  // app.color = 'red'
 
   // Handle routes and states
   var routes = {
     init: function(){
       // whats in the hash? / wat zit er in de link/# ?
-      var route = 'I don\'t know yet?' // hier moet miss de hash cange
+      var route = location.hash
       sections.toggle(route)
+
+      route != '' ? sections.toggle(route) : window.location.hash = '#start'
+
+      window.addEventListener('hashchange', function() {
+          sections.toggle(window.location.hash) // pakt de section met de # waar de link (aan het einde) naar veranderd
+      })
     }
   }
 
-  // Renden toggle sections
+
+  // Render toggle sections
   var sections = {
     toggle: function(route){
-      console.log(route)
 
+      var elements = document.querySelectorAll('section');
+
+      // loop-ed door alle sections heen & adds class .hidden
+      elements.forEach(function(el){
+        '#' + el.id === route ? el.classList.remove('hidden') : el.classList.add('hidden')
+      })
     }
   }
 
