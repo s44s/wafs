4d0649fd25e8209ebbe737e1d3a6900e80aa5ed3
Suus ten Voorde <suus.ten.voorde@hva.nl>
2018-02-12 15:23:18 +0100

    loading data from last.fm API and template engine with transparency.js

--- app/static/js/script0.2.js
+++ app/static/js/script0.2.js
@@ -15,30 +15,76 @@
 	// Handle routes & state
 	var routes = {
 		init: function(){
+			//implement routie
+
+			request.getData();
 			this.handleEvents()
 		},
 
 		handleEvents: function(){
 			window.addEventListener("load", function(){
 				var start = document.querySelector('section:first-of-type')
+				var songs = document.querySelector('#songs')
 				start.classList.add('active')
+
 			})
 
 			window.addEventListener("hashchange", function(){
 				var route = location.hash
-				sections.toggle(route)
+				template.toggle(route)
 				//console.log(location.hash)
 			})
 		}
 	}
 
+	var request = {
+		url: function(){
+			/* https://stackoverflow.com/questions/1714786/query-string-encoding-of-a-javascript-object */
+			var searchParams = new URLSearchParams()
+			var search =  {
+				method:'user.gettoptracks',
+				user: 'suustenvoorde',
+				api_key: 'd8454c3a14cf20d91a8213aa468bdb97',
+				format: 'json',
+				limit: '10',
+				period: '1month'
+			}
+			Object.keys(search).forEach(key => searchParams.append(key, search[key]))
+			return searchParams.toString()
+		},
+		getData: function(){
+			var request = new XMLHttpRequest()
+			request.open('GET', 'http://ws.audioscrobbler.com/2.0/?' + this.url(), true)
+
+			request.onload = function() {
+				if (request.status >= 200 && request.status < 400) {
+					var data = JSON.parse(request.responseText)
+					template.render(data)
+				} else {
+				 // We reached our target server, but it returned an error
+				}
+			};
+
+			request.onerror = function() {
+				// There was a connection error of some sort
+			};
+
+			request.send();
+		}
+	}
+
 	// Render / toggle section
-	var sections = {
+	var template = {
+		render: function (data) {
+			console.log(data)
+			console.log(data.toptracks.track)
+			var songs = document.querySelector('#songs ul')
+			var toptracks = data.toptracks.track
+
+			Transparency.render(songs, toptracks);
+		},
 		hide: function(){
 			var sections = document.querySelectorAll('section')
-			// for (var i=0; i < sections.length; i++) {
-			// 	sections[i].classList.remove("active")
-			// }
 			sections.forEach(function(element) {
 				element.classList.remove('active')
 			})
@@ -50,9 +96,7 @@
 		},
 
 		toggle: function(route){
-			// hide all sections
 			this.hide()
-			// hide all sections
 			this.show(route)
 		}
 	}
